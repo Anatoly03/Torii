d829fe9ac10c7ad5eda4037312ca18ee9459d71f
Anatoly Wein <anatoly.wein@gmail.com>
2026-06-13 14:54:43 +0200

    refactor

--- torii-client/src/views/HomeView.vue
+++ torii-client/src/views/HomeView.vue
@@ -4,21 +4,38 @@
             <div class="view-recent-projects">
                 {{ $t('placeholder.noRecentProjects') }}
             </div>
-            <LanguageSelect />
+            <div class="view-home-settings">
+                <LanguageSelect />
+            </div>
         </div>
-        <div class="view-home-content">{{ $t('menu.home') }}</div>
+        <div class="view-home-content">
+            {{ $t('menu.home') }}
+            <small class="version">
+                {{ $t('version.verbose', { version }) }}
+            </small>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { listRecentProjects } from '../composables/listRecentProjects';
+import { getVersion } from '@tauri-apps/api/app';
 
 import LanguageSelect from '@/components/LanguageSelect.vue';
 
 const i18n = useI18n();
 const recentProjects = computed(() => listRecentProjects());
+const version = ref(i18n.t('version.unknown'));
+
+onMounted(async () => {
+    try {
+        version.value = await getVersion();
+    } catch (error) {
+        console.error('Failed to get app version:', error);
+    }
+});
 </script>
 
 <style lang="scss" scoped>
@@ -32,6 +49,7 @@ const recentProjects = computed(() => listRecentProjects());
         flex-direction: column;
         width: 200px;
         padding: 16px;
+        gap: 8px;
         border-right: 1px solid #ccc;
 
         .view-recent-projects {
@@ -40,6 +58,20 @@ const recentProjects = computed(() => listRecentProjects());
             color: #666;
             flex: 1;
         }
+
+        .view-home-settings {
+            display: flex;
+            flex-direction: column;
+            gap: 8px;
+        }
+    }
+
+    .view-home-content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        flex: 1;
     }
 }
 </style>
