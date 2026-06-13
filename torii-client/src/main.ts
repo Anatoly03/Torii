c6ffe0a262723069a4ece8ed5ec4737d1b734ebd
Anatoly Wein <anatoly.wein@gmail.com>
2026-06-13 09:52:09 +0200

    refactor

--- torii-client/src/main.ts
+++ torii-client/src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
+import App from './App.vue';
+import router from './router';
+import en from './locales/en.json';
 
-createApp(App).use(router).mount("#app");
+const i18n = createI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages: { en },
+});
+
+createApp(App).use(router).use(i18n).mount('#app');
