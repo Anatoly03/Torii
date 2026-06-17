4682355918a90172cae62a82b33cdb39347efd05
Anatoly Wein <anatoly.wein@gmail.com>
2026-06-17 12:04:41 +0200

    add app language setting

--- torii-client/src/components/LanguageSelect.vue
+++ torii-client/src/components/LanguageSelect.vue
@@ -1,7 +1,13 @@
 <template>
-    <select v-model="$i18n.locale">
+    <select v-model="$i18n.locale" class="locale-select">
         <option value="en">English</option>
         <option value="jp">日本語</option>
         <option value="zh">中文</option>
     </select>
 </template>
+
+<style lang="scss" scoped>
+.locale-select {
+    flex: 1;
+}
+</style>
--- torii-client/src/views/settings/GeneralSettingsView.vue
+++ torii-client/src/views/settings/GeneralSettingsView.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="view-settings-general">
-        {{ $t('placeholder.generalSettings') }}
-    </div>
+    <!-- <div class="view-settings-general"> -->
+        <n-form label-placement="left" label-width="auto">
+            <n-form-item label="Language">
+                <LanguageSelect />
+            </n-form-item>
+        </n-form>
+    <!-- </div> -->
 </template>
+
+<script setup lang="ts">
+import LanguageSelect from '../../components/LanguageSelect.vue';
+import { NForm, NFormItem } from 'naive-ui';
+</script>
 
 <style lang="scss" scoped>
 .view-settings-general {
     display: flex;
     flex: 1;
     flex-direction: column;
-    align-items: center;
+    // align-items: center;
 }
 </style>
