8390a014a5af8456c8ac3e75cb78a91af80c1d21
Anatoly Wein <anatoly.wein@gmail.com>
2026-06-19 12:56:55 +0200

    refactor markdown editor

--- torii-client/src/views/project/ProjectView.vue
+++ torii-client/src/views/project/ProjectView.vue
@@ -17,10 +17,10 @@
         <div class="view-project-content-placeholder" v-if="!currentFile">
             Project View: {{ projectPath }}<br />
         </div>
-        <editor-content
+        <MarkownEditor
             class="view-project-content"
+            :file="currentFile"
             v-else
-            :editor="editor"
         />
     </div>
 </template>
@@ -29,59 +29,17 @@
 import { onUnmounted, ref, watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import FileTree from '../../components/file/FileTree.vue';
-import { invoke } from '@tauri-apps/api/core';
-import { Editor, EditorContent } from '@tiptap/vue-3';
-import { Markdown } from '@tiptap/markdown';
-import StarterKit from '@tiptap/starter-kit';
+import MarkownEditor from './MarkownEditor.vue';
 
 const route = useRoute();
 const router = useRouter();
 const projectPath = route.params.projectPath as string;
 const currentFile = ref<string | null>(null);
-const ignoreFirstSave = ref(true);
-const editor = new Editor({
-    extensions: [Markdown, StarterKit],
-    content: "<p>I'm running Tiptap with Vue.js. 🎉</p>",
-    onUpdate: saveFile,
-});
 
 if (!projectPath) {
     // If no project path is provided, redirect to the home page
     router.push('/');
 }
-
-watch(currentFile, async (path) => {
-    if (!path) return;
-
-    // Load the file content when a new file is selected
-    let content = await invoke<string>('get_markdown_file', { path });
-    console.debug('load', content);
-
-    ignoreFirstSave.value = true;
-    editor.commands.setContent(content, { contentType: 'markdown' });
-});
-
-async function saveFile() {
-    if (!currentFile.value) return;
-    if (ignoreFirstSave.value) {
-        ignoreFirstSave.value = false;
-        return;
-    }
-
-    // Get markdown content from the editor
-    const content = editor.getMarkdown();
-    console.debug('save', content);
-
-    // Save the file content whenever it changes
-    await invoke('save_markdown_file', {
-        path: currentFile.value,
-        content,
-    });
-}
-
-onUnmounted(() => {
-    editor.destroy();
-});
 </script>
 
 <style lang="scss" scoped>
